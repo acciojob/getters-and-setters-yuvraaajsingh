1e6da37a48840b2ba7cd0b269eadd3dac06c1606
Yuvraj Singh <84833138+yuvraaajsingh@users.noreply.github.com>
2025-05-13 01:08:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,49 @@
-//complete this code
-class Person {}
+// Person Class
+class Person {
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
 
-class Student extends Person {}
+  // Getter for name
+  get name() {
+    return this._name;
+  }
 
-class Teacher extends Person {}
+  // Getter for age
+  get age() {
+    return this._age;
+  }
+
+  // Setter for age
+  set age(age) {
+    this._age = age;
+  }
+}
+
+// Student Class (inherits from Person)
+class Student extends Person {
+  constructor(name, age) {
+    super(name, age);  // Call the parent constructor
+  }
+
+  // Method for the student to study
+  study() {
+    console.log(`${this.name} is studying`);
+  }
+}
+
+// Teacher Class (inherits from Person)
+class Teacher extends Person {
+  constructor(name, age) {
+    super(name, age);  // Call the parent constructor
+  }
+
+  // Method for the teacher to teach
+  teach() {
+    console.log(`${this.name} is teaching`);
+  }
+}
 
 // Do not change the code below this line
 window.Person = Person;
